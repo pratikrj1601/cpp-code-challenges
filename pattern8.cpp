f6f5e180b1bf501013379be7838010832f073bf5
pratik joshi <83777309+pratikrj1601@users.noreply.github.com>
2023-09-25 05:27:17 +0530

    Update pattern8.cpp

--- pattern8.cpp
+++ pattern8.cpp
@@ -9,23 +9,29 @@ using namespace std;
 // * * * *
 // * * * * *
 
-// int main() {
+/* First approach: we are observing that row number from 1 to n (n is an integer value given by user) and in each row there is row number of columns.
+// For Ex. first row has 1 column, 2nd has 2 columns, 3rd has 3 columns and so on.... */
 
-//     int n, i=1;
-//     cin>>n;
-
-//     while (i<=n) {
-//         int j = 1;        
-//         while (j<=i) {
-//             cout<<"* "; 
-//             j++;
-//         }
-//         i++;
-//         cout<<endl;
-//     }
-// }
 
 int main() {
+
+    int n, i=1;
+    cin>>n;
+
+    while (i<=n) {
+        int j = 1;        
+        while (j<=i) {
+            cout<<"* "; 
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+}
+
+// Alternate approach: Iterate both rows and columns till given number and print * only if row value is greater or equal to column value.
+
+/* int main() {
 
     int n, i=1;
     cin>>n;
@@ -42,4 +48,4 @@ int main() {
         i++;
         cout<<endl;
     }
-}
+} */
