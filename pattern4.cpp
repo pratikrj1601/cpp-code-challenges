c3e90b96bcd39a27eac46b082c713322d38a02ec
pratik joshi <83777309+pratikrj1601@users.noreply.github.com>
2023-09-25 01:46:00 +0530

    Updated pattern4.cpp

--- pattern4.cpp
+++ pattern4.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 // Printing patterns using conditionals and if loops
 
-// 1 2 3 4
-// 1 2 3 4
-// 1 2 3 4
-// 1 2 3 4
+// 4 4 4 4
+// 3 3 3 3
+// 2 2 2 2
+// 1 1 1 1
 
 int main() {
 
