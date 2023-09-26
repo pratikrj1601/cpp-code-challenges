99172941d1f01ecced3dcd9f07e854661da936d3
pratik joshi <83777309+pratikrj1601@users.noreply.github.com>
2023-09-26 19:00:10 +0530

    Update pattern27.cpp

--- Pattern-21to30/pattern27.cpp
+++ Pattern-21to30/pattern27.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 // Printing patterns using conditionals and if loops
 
-//       4
-//     3 4
-//   2 3 4
-// 1 2 3 4
+//       4 
+//     3 4 
+//   2 3 4 
+// 1 2 3 4 
 
 /* Approach: we are observing that row number starts from 1 to n (n is an integer value given by user) and in each row there is i number of columns and n-i spaces.
 // For Ex. first row has 1 column and 3 spaces, 2nd row has 2 columns and 2 spaces, 3rd has 2 columns and so on.... and in each row we are printing value of n-i+j. */
